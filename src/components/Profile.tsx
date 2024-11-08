07ad4b5e328cb3fd9663fe11b0bdd3e895e77213
Andy <andychert@protonmail.com>
2024-11-08 12:10:49 +0200

    Add: Ton Connect

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@/components/ui/button";
 import {
   ConnectedWallet,
   TonConnectButton,
   useTonConnectUI,
 } from "@tonconnect/ui-react";
-import React, { useEffect, useState } from "react";
-import { FaSave, FaUserCircle } from "react-icons/fa";
+import React, { useCallback, useEffect, useState } from "react";
+import { FaUserCircle } from "react-icons/fa";
 
 interface ProfileProps {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -15,62 +14,59 @@ interface ProfileProps {
 
 const Profile: React.FC<ProfileProps> = ({ userData, initData }) => {
   const [tonAddress, setTonAddress] = useState(userData.tonAddress || "");
-  const [isSaving, setIsSaving] = useState(false);
   const [tonConnectUI] = useTonConnectUI();
+
+  const saveAddressToDatabase = useCallback(
+    async (address: string) => {
+      try {
+        const response = await fetch(
+          "https://d2xcq5opb5.execute-api.us-east-1.amazonaws.com/user",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              initData: initData,
+              action: "update",
+              updateData: {
+                tonAddress: address,
+              },
+            }),
+          }
+        );
+
+        const data = await response.json();
+
+        if (response.ok) {
+          window.Telegram.WebApp.showAlert("TON Address successfully saved!");
+        } else {
+          console.error("Error saving address:", data.error);
+          window.Telegram.WebApp.showAlert(`Error: ${data.error}`);
+        }
+      } catch (error) {
+        console.error("Error saving address:", error);
+        window.Telegram.WebApp.showAlert("An unexpected error occurred.");
+      }
+    },
+    [initData]
+  );
 
   useEffect(() => {
     tonConnectUI.onStatusChange((status) => {
       if (status && (status as ConnectedWallet).account) {
         const address = (status as ConnectedWallet).account.address;
         setTonAddress(address);
-        window.Telegram.WebApp.showAlert(`Connected to TON Wallet: ${address}`);
+        saveAddressToDatabase(address);
       } else {
         setTonAddress("");
         window.Telegram.WebApp.showAlert("Disconnected from TON Wallet.");
       }
     });
-  }, [tonConnectUI]);
+  }, [saveAddressToDatabase, tonConnectUI]);
 
-  const handleSaveProfile = async () => {
-    if (!tonAddress) {
-      window.Telegram.WebApp.showAlert("Please connect your TON wallet first!");
-      return;
-    }
-
-    setIsSaving(true);
-    try {
-      const response = await fetch(
-        "https://d2xcq5opb5.execute-api.us-east-1.amazonaws.com/user",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            initData: initData,
-            action: "update",
-            updateData: {
-              tonAddress: tonAddress,
-            },
-          }),
-        }
-      );
-
-      const data = await response.json();
-
-      if (response.ok) {
-        console.log("Profile updated");
-        window.Telegram.WebApp.showAlert("Profile successfully updated!");
-      } else {
-        console.error("Error updating profile:", data.error);
-        window.Telegram.WebApp.showAlert(`Error: ${data.error}`);
-      }
-    } catch (error) {
-      console.error("Error updating profile:", error);
-      window.Telegram.WebApp.showAlert("An unexpected error occurred.");
-    } finally {
-      setIsSaving(false);
-    }
+  const formatAddress = (address: string) => {
+    return `${address.slice(0, 6)}...${address.slice(-6)}`;
   };
 
   return (
@@ -91,27 +87,17 @@ const Profile: React.FC<ProfileProps> = ({ userData, initData }) => {
         </p>
         <p className="text-sm mb-4">@{userData.username || `anonymous`}</p>
       </div>
-      <div className="w-full max-w-md">
-        <label htmlFor="tonAddress" className="block text-sm mb-1">
-          TON Address:
-        </label>
-        <input
-          id="tonAddress"
-          type="text"
-          value={tonAddress}
-          readOnly
-          className="bg-gray-800 text-white p-2 rounded w-full text-sm focus:outline-none opacity-90 bg-teal-700/10 shadow-md"
-        />
+      <div className="w-full max-w-md text-center">
+        <p className="block text-sm mb-1">TON Address:</p>
+        {tonAddress ? (
+          <p className="bg-gray-800 text-white p-2 rounded text-sm opacity-90 bg-teal-700/10 shadow-md">
+            {formatAddress(tonAddress)}
+          </p>
+        ) : (
+          <p className="text-gray-500">Not connected</p>
+        )}
       </div>
       <TonConnectButton />
-      <Button
-        onClick={handleSaveProfile}
-        className="mt-4 text-sm flex items-center focus:outline-none hover:opacity-90 opacity-80 bg-teal-700/10 shadow-md"
-        disabled={isSaving}
-      >
-        {isSaving ? "Saving..." : "Save Profile"}
-        <FaSave className="ml-2" />
-      </Button>
     </div>
   );
 };
