f6ed66bc6c60147dd1f2eac56cb9b216918dec1a
Andy <andychert@protonmail.com>
2024-11-09 21:27:36 +0200

    Add: vite-plugin-node-polyfills

--- src/components/game/elements/Header.tsx
+++ src/components/game/elements/Header.tsx
@@ -13,14 +13,18 @@ const Header: React.FC<HeaderProps> = ({
   return (
     <header className="absolute top-0 p-2 w-full flex items-center justify-center z-20">
       <div className="absolute left-2 flex items-center">
-        <img src={`logo.svg`} className="opacity-80 h-16 w-auto" alt="Logo" />
+        <img
+          src={`logo.svg`}
+          className="opacity-80 h-16 w-auto"
+          alt="MazeTon"
+        />
       </div>
 
       <div className="flex flex-col items-center text-lg relative font-['Tektur'] overflow-hidden w-full">
         <div className="absolute inset-0 flex items-center justify-center text-6xl text-white opacity-5 font-['Rubik_Maze']">
           {score}
         </div>
-        {score} <div className="text-xs text-green-400">MAZE</div>
+        {score} <div className="text-xs text-green-400">$MAZE</div>
       </div>
 
       <div className="absolute right-2 flex gap-2 items-center">
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [nodePolyfills(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
